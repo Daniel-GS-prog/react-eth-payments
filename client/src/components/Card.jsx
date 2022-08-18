9e8c108d7dc717769ffe3bedf89b27bf4e45d350
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2022-08-18 14:02:05 +0100

    chagnges to card component.

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import './Card.css';
 
-const address = '0xE7db30F0831Bae94e37209ddBD3d74fFAacBD1bc';
 
-function copy() {
-	/* Get the text field */
-	var copyText = document.getElementById("myInput");
-  
-	// /* Select the text field */
-	copyText.select();
-	copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-	 /* Copy the text inside the text field */
-	navigator.clipboard.writeText(copyText.value);
-  
-	/* Alert the copied text */
-	alert("Copied the text: " + copyText.value);
-  } 
 
 const Card = ({name, email, id}) => {
 
+	const address = '0xE7db30F0831Bae94e37209ddBD3d74fFAacBD1bc';
+
+
+
 
 	return (
+		
 
 		<div className=' Card black '>
 			<img src={`https://robohash.org/${id}`} alt='User' />
@@ -30,10 +20,10 @@ const Card = ({name, email, id}) => {
 
 				<p>{email}</p>
 
-				
-					<p>Eth address:</p>
-					<p>{address.substring(0,8)}...</p>
-				
+				<div className='display-flex'>
+					<p>Eth address: {address.substring(0,8)}...</p>
+					
+				</div>
 
 				
 				
